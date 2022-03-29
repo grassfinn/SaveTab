50f013034fcafcec54936b7a4d53d14a4d74a8a3
grassfinn <theharper@live.com>
2022-03-29 19:53:07 -0400

    Grab current tab

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const saveInput = document.querySelector('#save');
 
 const deleteBtn = document.querySelector('#delete-all');
 
+const saveTab = document.querySelector('#save-tab');
+
 // parse converts string to obj stringify turns into a string
 // turn local storage into obj
 const readLaterLocalStorage = JSON.parse(localStorage.getItem('readLater'));
@@ -18,14 +20,13 @@ const readLaterLocalStorage = JSON.parse(localStorage.getItem('readLater'));
 
 // console.log(readLaterLocalStorage);
 
-
 // check if readLaterLocalStorage is truthy on site load/refresh
 if (readLaterLocalStorage) {
   // set readLater to its value and call renderList()
   readLater = readLaterLocalStorage;
   console.log('This is your list', readLater);
   // render the items based upon the array
-  render(readLater);
+  renderList();
 }
 
 // double click event
@@ -33,6 +34,20 @@ deleteBtn.addEventListener('dblclick', () => {
   localStorage.clear();
   readLater = [];
   list.textContent = '';
+});
+
+
+// Save Tab
+saveTab.addEventListener('click', () => {
+  // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {})
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    console.log(tabs);
+    // grab url of the tab
+    readLater.push(tabs[0].url);
+    localStorage.setItem('readLater', JSON.stringify(readLater));
+  });
+  list.textContent = '';
+  render(readLater);
 });
 
 // (value, key) local storage only takes in strings
@@ -57,8 +72,8 @@ function renderList() {
 }
 //  For Each
 // renders the whole list and keeps track of the local store
-function render(articles) {
-  articles.forEach((item) => {
+function render(arr) {
+  arr.forEach((item, index) => {
     // create a list item and append it the UL element
     const listItem = document.createElement('li');
     list.append(listItem);
@@ -76,12 +91,13 @@ saveInput.addEventListener('click', () => {
   if (inputEl.value) {
     // push the text input inside of the readLater array
     readLater.push(inputEl.value);
+    list.textContent = '';
     // set the local storage and turn it into a string
     localStorage.setItem('readLater', JSON.stringify(readLater));
-    // render the item
-    renderList();
-    // reset the text input value
     inputEl.value = '';
+    // reset the text input value
+    // render the item
+    render(readLater);
 
     // have to use '' for get item
     // console.log(localStorage.getItem('readLater'));
